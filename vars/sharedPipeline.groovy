ef91c55c5a290bcb8f09f78b88757f4999470833
xuzan <gitxuzan@gmail.com>
2023-10-12 16:53:09 +0800

    initial commit

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -117,6 +117,7 @@ def call(Map params){
                        expression { CURL_URL != ""  && env.CURL_SLEEP.toInteger() > 0 }
                    }
                     script {
+                        sleep env.CURL_SLEEP.toInteger()
                         printn("睡眠${env.CURL_SLEEP}s,在验证http!")
                         sh """
                             status_code=\$(curl -o /dev/null -s -w "%{http_code}" ${CURL_URL})
