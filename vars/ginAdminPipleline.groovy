ee2d66b37f35da15e906d641f51aacef289414b6
xuzan <gitxuzan@gmail.com>
2023-12-04 18:00:25 +0800

    #commit

--- vars/ginAdminPipleline.groovy
+++ vars/ginAdminPipleline.groovy
@@ -126,7 +126,7 @@ def call(Map params){
                            /usr/local/go/bin/go mod tidy
                            CGO_ENABLED=0 GOOS=linux GOARCH=amd64 /usr/local/go/bin/go build -x -ldflags "-s -w" -o ./local_news_gva ./main.go
                          '''
-                    if (env.OPERATION == "deploy_web") {
+                    }else if (env.OPERATION == "deploy_web") {
                        sh '''
                             export PATH=/usr/local/node-v20.10.0/bin:$PATH
                             cd ./web
