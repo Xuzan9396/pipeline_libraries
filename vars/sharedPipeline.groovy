70502d4338df48d6eba6158958ac8fe72e1628ab
xuzan <gitxuzan@gmail.com>
2023-12-15 18:31:09 +0800

    #commit

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -99,6 +99,8 @@ def call(Map params){
                     script {
                         sh 'docker build --platform linux/amd64 -t gitxuzan/${CRAWLER_API}:${VERSION} -f Dockerfile_amd64_arm64 .'
                         sh 'docker push gitxuzan/${CRAWLER_API}:${VERSION}'
+                        // 删除无用的镜像
+                        sh 'docker rmi $(docker images -f "dangling=true" -q)'
                     }
                 }
             }
